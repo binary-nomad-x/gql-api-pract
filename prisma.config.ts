534e395e0ff0c738cb32e6479096ab842616e933
ddanish <user@example.com>
2026-05-14 00:10:21 +0500

    chagnes to make project up and running

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,18 +1,17 @@
-import 'dotenv/config';
-import { defineConfig } from 'prisma/config';
+import "dotenv/config";
+import { defineConfig } from "prisma/config";
 
 const databaseUrl = process.env.DATABASE_URL;
 
 if (!databaseUrl) {
-  throw new Error('DATABASE_URL is not defined in the environment variables.');
+  throw new Error("DATABASE_URL is not defined in the environment variables.");
 }
 
 export default defineConfig({
-  schema: 'prisma/schema.prisma',
+  schema: "prisma/schema.prisma",
   migrations: {
-    path: 'prisma/migrations',
+    path: "prisma/migrations",
   },
-  // for prisma 7+ or cli tools, add this connection to here
   datasource: {
     url: databaseUrl,
   },
